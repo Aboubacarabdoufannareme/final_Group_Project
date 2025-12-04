a5ce22f8f83c9e96e9fb3096ec056a70bee2df2a
Fannareme Aboubacar Abdou <125912257+Aboubacarabdoufannareme@users.noreply.github.com>
2025-12-04 23:11:47 +0900

    Update Login.php

--- Login.php
+++ Login.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-require_once 'DBconnection.php';
+require_once __DIR__ . '/../config/dbconnection.php'; // adjust path based on folder structure
+
 
 // If already logged in, redirect
 if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
